c646d5c25c7d7ee796512a5a7a07129249429358
Saksham <saksham.8484@gmail.com>
2022-02-09 19:46:18 +0530

    refractor: links sent to css from html

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const menu = document.querySelector("#menu");
-const list = document.querySelector(".links");
+const list = document.querySelector(".nav-links");
 const close = document.querySelector("#close-btn");
 menu.addEventListener("click", () => {
     list.classList.add("show")
@@ -10,3 +10,14 @@ close.addEventListener("click", () => {
     list.classList.remove("show")
     list.classList.add("hide") 
 })
+
+function preFormatter() {
+    const code = document.querySelectorAll(".exec-code");
+    for (let i of code) {
+    let x = i.innerHTML;
+    x.replaceAll("<", "&lt;");
+    x.replaceAll(">", "&gt;\n");
+    i.textContent = x;
+    }
+    }
+    preFormatter();
